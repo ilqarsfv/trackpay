cfa96c705c5ae23d9f2070585d95348d4ad35a1d
ilqarsfv <ilqar.sofiyev.95@gmail.com>
2025-09-21 22:42:39 +0400

    Yeni contentin əlavə edilməsi ve dizaynda deyisikliklerin edilmesi

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,13 @@
+$(document).ready(function () {
+    $(".buttonSocials").click(function () {
+        $("#socialsWrapper,#openModal").toggleClass("active");
+    })
+    $("#openModal,#closeModal").click(function () {
+        $("#modalWrapper").toggleClass("active");
+    })
+})
+
+
 document.addEventListener("scroll", function () {
     const header = document.querySelector("header");
     if (window.scrollY > 40) {
@@ -5,4 +15,5 @@ document.addEventListener("scroll", function () {
     } else {
         header.classList.remove("active");
     }
+
 });
